ec30668ae375ae49bca24946d63dc3d71bba41bd
50616006 <50616006@50616006-pc>
2018-01-15 17:48:35 +0900

    menu

---
+++ src/menu/Menu_main.java
@@ -0,0 +1,16 @@
+package menu;
+
+import menu.Menu_view;
+
+public class Menu_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Menu_view frame = new Menu_view();
+		frame.setBounds(5,5,750,750);
+		frame.setVisible(true);
+		
+	}
+
+}
